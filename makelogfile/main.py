39c81c123c1c099ef8afca389265468cc96b0ebf
kuritake <53284909+kuritake@users.noreply.github.com>
2019-08-06 11:41:59 +0900

    Create main.py

---
+++ makelogfile/main.py
@@ -0,0 +1,8 @@
+import modules
+import logging
+
+if __name__ == "__main__":
+    log = logging.getLogger(__name__)
+    logging.info('info')
+    print("A log is recorded.")
+    logging.info('info')
